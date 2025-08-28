5d0d23ec5c88b564ea7bb1742805cbd341f8da21
Moïse Aganze Lwaboshi <moiseaganze76@gmail.com>
2025-08-28 19:52:00 +0200

    Ajout dashboard.js ...

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://xxxx.supabase.co";
-const SUPABASE_ANON_KEY = "ton_anon_key";
+const SUPABASE_URL = "https://ohqgbojfjjowjayycpph.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ocWdib2pmampvd2pheXljcHBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzODk5NzQsImV4cCI6MjA3MTk2NTk3NH0.9Av1dlKszmkrX3z81m3pAB4fZARXH0PcxG_TLSXfsK8";
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const loginDiv = document.getElementById("login");
